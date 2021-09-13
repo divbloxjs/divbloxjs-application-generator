22d458568c6dd2cdaf5bb77313a43e8e3a4d9455
Johan Griesel <johan@stratusolve.com>
2021-09-13 12:16:57 +0200

    Better template paths

--- bin/init-dx.js
+++ bin/init-dx.js
@@ -8,6 +8,7 @@ const dataModelFileName = divbloxConfigRoot+'data-model.json';
 const dxConfigFileName = divbloxConfigRoot+'dxconfig.json';
 const dxExampleScriptFileName = divbloxRoot+'divblox-example.js';
 const dxInitFileName = divbloxConfigRoot+'dx-init.js';
+const TEMPLATE_DIR = path.join(__dirname, '..', 'templates')
 
 /**
  * Creates the minimum configuration files needed for Divblox to be initiated
@@ -15,29 +16,28 @@ const dxInitFileName = divbloxConfigRoot+'dx-init.js';
  */
 async function createDefaults() {
     console.log("Initializing Divblox...");
-    return;
     if (!fs.existsSync(divbloxConfigRoot)){
         console.log("Creating Divblox config directory...");
         fs.mkdirSync(divbloxConfigRoot);
     }
     if (!fs.existsSync(dataModelFileName)) {
         console.log("Creating Divblox data model...");
-        const dxDataModelDefaultStr = await fsAsync.readFile('./templates/data-model.json');
+        const dxDataModelDefaultStr = await fsAsync.readFile(TEMPLATE_DIR+'/data-model.json');
         await fsAsync.writeFile(dataModelFileName, dxDataModelDefaultStr);
     }
     if (!fs.existsSync(dxConfigFileName)) {
         console.log("Creating Divblox default config file...");
-        const dxConfigDefaultStr = await fsAsync.readFile('./templates/dxconfig.json');
+        const dxConfigDefaultStr = await fsAsync.readFile(TEMPLATE_DIR+'/dxconfig.json');
         await fsAsync.writeFile(dxConfigFileName, dxConfigDefaultStr);
     }
     if (!fs.existsSync(dxInitFileName)) {
         console.log("Creating Divblox custom init file...");
-        const dxInitStr = await fsAsync.readFile('./templates/dx-init.js');
+        const dxInitStr = await fsAsync.readFile(TEMPLATE_DIR+'/dx-init.js');
         await fsAsync.writeFile(dxInitFileName, dxInitStr);
     }
     if (!fs.existsSync(dxExampleScriptFileName)) {
         console.log("Creating Divblox example script...");
-        const dxExampleScriptStr = await fsAsync.readFile('./templates/divblox-example.js');
+        const dxExampleScriptStr = await fsAsync.readFile(TEMPLATE_DIR+'/divblox-example.js');
         await fsAsync.writeFile(dxExampleScriptFileName, dxExampleScriptStr);
     }
     console.log("Done!");
