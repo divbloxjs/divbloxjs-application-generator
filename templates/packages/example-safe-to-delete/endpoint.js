95b892069d22494723294c64ada829effbb2a97f
Johan Griesel <johan@stratusolve.com>
2021-09-21 18:15:53 +0200

    Update endpoint.js

--- templates/packages/example-safe-to-delete/endpoint.js
+++ templates/packages/example-safe-to-delete/endpoint.js
@@ -1,1 +1,10 @@
 const dx = require('../../dx-app');
+const divbloxEndpointBase = require('divbloxjs/dx-core-modules/endpoint-base');
+
+class ExampleSafeToDelete extends divbloxEndpointBase {
+    constructor() {
+        super();
+    }
+}
+const ExampleSafeToDeleteInstance = new ExampleSafeToDelete();
+module.exports = ExampleSafeToDeleteInstance;
