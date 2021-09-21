e26c247ccb2a2e01b7b780024f21efdf705c34a7
Johan Griesel <johan@stratusolve.com>
2021-09-21 18:15:26 +0200

    Update api.js

--- templates/routes/api.js
+++ templates/routes/api.js
@@ -2,9 +2,20 @@ const express = require('express');
 const router = express.Router();
 const dx = require('../dx-app');
 
-router.get('/', async (req, res, next) => {
-    // res.send('Test route served');
+router.all('/', async (req, res, next) => {
+    //TODO: This must be updated to look nicer
     res.render('index', { title: 'Divblox API Root' });
 });
+
+for (const packageName of Object.keys(dx.packages)) {
+    const packageObj = dx.packages[packageName];
+    const packageEndpoint = require('../'+packageObj.packageRoot+"/endpoint");
+    for (const operation of packageEndpoint.declaredOperations) {
+        router.all('/'+packageName+'/'+operation, async (req, res, next) => {
+            const result = await packageEndpoint.executeOperation(operation, {"headers":req.headers,"body":req.body,"query":req.query});
+            res.send(result);
+        });
+    }
+}
 
 module.exports = router;
