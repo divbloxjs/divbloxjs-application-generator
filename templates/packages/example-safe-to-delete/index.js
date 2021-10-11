bc2eb8d36220202f0d462f9a275d3ef205ab9964
Johan Griesel <johan@stratusolve.com>
2021-10-11 14:16:25 +0200

    Some minor clean up

--- templates/packages/example-safe-to-delete/index.js
+++ templates/packages/example-safe-to-delete/index.js
@@ -13,16 +13,16 @@ class ExampleSafeToDelete extends divbloxPackageControllerBase {
         if ((typeof dx === "undefined") || (dx === null)) {
             throw new Error("Divblox instance was not provided");
         }
-        // Let's create a new row for the object of type "exampleEntity" in the database with some parameters
-        const objId = await dx.create("exampleEntity", {"exampleDateTime":"2021-01-01 12:00:00","exampleString":"An example string","exampleNumber":123,"exampleText":"Example text"});
+        // Let's create a new row for the object of type "exampleEntityOne" in the database with some parameters
+        const objId = await dx.create("exampleEntityOne", {"exampleOneTimeStamp":"2021-01-01 12:00:00","exampleOneStringWithoutNull":"Example String","exampleOneBigInt":123,"exampleOneText":"Example text"});
         if (objId === -1) {
-            console.log("Failed to create new exampleEntity: "+JSON.stringify(dx.getError()));
+            console.log("Failed to create new exampleEntityOne: "+JSON.stringify(dx.getError()));
         } else {
             // Divblox will always return the database table id for the newly created entry
-            console.log("New exampleEntity created!");
+            console.log("New exampleEntityOne created!");
 
-            // Let's read the entry from the database. This basically performs a "SELECT from `exampleEntity` WHERE `id` = objId"
-            const obj = await dx.read("exampleEntity", objId);
+            // Let's read the entry from the database. This basically performs a "SELECT from `exampleEntityOne` WHERE `id` = objId"
+            const obj = await dx.read("exampleEntityOne", objId);
             if (obj !== null) {
                 console.log("Found: "+JSON.stringify(obj, null, 2));
             } else {
@@ -30,14 +30,14 @@ class ExampleSafeToDelete extends divbloxPackageControllerBase {
             }
 
             // Let's try and change something on this object using the "update" function
-            if (!await dx.update("exampleEntity", {"id":objId, "exampleString":"An updated string", "exampleNumber":999})) {
+            if (!await dx.update("exampleEntityOne", {"id":objId, "exampleOneStringWithNull":"An updated string", "exampleOneBigInt":999})) {
                 console.log("Error updating: "+JSON.stringify(dx.getError()));
             } else {
                 console.log("Updated!");
             }
 
-            //Let's try deleting an account using the "delete" function and specifying the exampleEntity's id
-            if (!await dx.delete("exampleEntity", 2)) {
+            //Let's try deleting an account using the "delete" function and specifying the exampleEntityOne's id
+            if (!await dx.delete("exampleEntityOne", 2)) {
                 console.log("Error deleting: "+JSON.stringify(dx.getError()));
             } else {
                 console.log("Deleted!");
