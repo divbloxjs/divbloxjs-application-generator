c687bf380eed2df039e19f8478aedb18ceec5dbc
Johan Griesel <johan@stratusolve.com>
2021-11-09 10:39:37 +0200

    Update endpoint.js

--- templates/packages/example-safe-to-delete/endpoint.js
+++ templates/packages/example-safe-to-delete/endpoint.js
@@ -5,17 +5,31 @@ const divbloxEndpointBase = require('divbloxjs/dx-core-modules/endpoint-base');
 class ExampleSafeToDelete extends divbloxEndpointBase {
     constructor() {
         super();
-        this.endpointName = "example-endpoint";
+        this.endpointName = "my-example";
         this.endpointDescription = "An example endpoint to demonstrate divblox api's";
 
         // We add a custom operation declaration here
         const testOperation = this.getOperationDefinition(
             {
                 "operationName": "test",
-                "allowedAccess": ["anonymous"]
+                "allowedAccess": ["anonymous"],
+                "operationDescription": "This sentence describes the operation",
+                "parameters": [
+                    this.getInputParameter({"name":"test","type":"header"}),
+                    this.getInputParameter({"name":"test2","type":"path"})
+                ],
+                "requestType": "POST",
+                "requestSchema": this.getSchema({"testAttr":"string","testAttr2":"boolean"}),
+                "responseSchema": this.getSchema({"respAttr":"string","respAttr2":"boolean"})
             }
         );
+
+        // An example of how to declare operations. You need to do this in order for the operation to be available on
+        // the endpoint. Also, this declaration provides the necessary input for swagger ui present the docs for this
         this.declareOperations([testOperation]);
+
+        // An example of how to declare entity schemas for swagger ui
+        this.declareEntitySchemas(["globalIdentifier"]);
     }
 
     async executeOperation(operation, request, dxInstance = null) {
