bdaef7dfca2a3559aa2e4c013fbf4ba6ab085308
Johan Griesel <johan@stratusolve.com>
2021-11-09 10:45:25 +0200

    Update dx-manage-identifier-groupings.js

--- templates/dx-manage-identifier-groupings.js
+++ templates/dx-manage-identifier-groupings.js
@@ -10,15 +10,19 @@ function getOperation() {
     if (process.argv.includes("show")) {
         return "show";
     }
+
     if (process.argv.includes("create")) {
         return "create";
     }
+
     if (process.argv.includes("modify")) {
         return "modify";
     }
+
     if (process.argv.includes("remove")) {
         return "remove";
     }
+
     console.log("Provide an operation please show|create|modify|remove");
     return "show";
 }
