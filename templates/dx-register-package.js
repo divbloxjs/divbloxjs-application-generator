917e097dcd73aad5a20678a6d68e805fbd535bd8
Johan Griesel <johan@stratusolve.com>
2021-11-09 12:11:15 +0200

    Update dx-register-package.js

--- templates/dx-register-package.js
+++ templates/dx-register-package.js
@@ -1,1 +1,3 @@
 console.log("To be implemented");
+//TODO: This script must take input arguments that tell it to either register only, or to install first and then register
+//TODO: This script must call the function in divbloxjs "registerRemotePackage" to register the package
